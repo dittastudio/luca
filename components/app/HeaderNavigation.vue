faa54f458c0ccc83d09d642d476e45a60a1f488b
Will The Dev <hello@willthe.dev>
2024-04-19 11:46:10 +0100

    inner div added back in

--- components/app/HeaderNavigation.vue
+++ components/app/HeaderNavigation.vue
@@ -11,22 +11,19 @@ const items = computed(() => list?.items ?? [])
 </script>
 
 <template>
-  <div
-    data-component="AppHeaderNavigation"
-    class="app-header-navigation"
-    :class="{ 'app-header-navigation--is-open': isOpen }"
-  >
-    <ul class="app-header-navigation__list type-h2">
-      <li
-        v-for="(item, index) in items"
-        class="app-header-navigation__item"
-        :style="`--link-transition-delay: ${200 + index * 40}ms`"
-      >
-        <StoryblokLink class="app-header-navigation__link" :item="item.link">
-          {{ item.title }}
-        </StoryblokLink>
-      </li>
-    </ul>
+  <div data-component="AppHeaderNavigation" class="app-header-navigation"
+    :class="{ 'app-header-navigation--is-open': isOpen }">
+    <div class="app-header-navigation__inner">
+
+      <ul class="app-header-navigation__list type-h2">
+        <li v-for="(item, index) in items" class="app-header-navigation__item"
+          :style="`--link-transition-delay: ${200 + index * 40}ms`">
+          <StoryblokLink class="app-header-navigation__link" :item="item.link">
+            {{ item.title }}
+          </StoryblokLink>
+        </li>
+      </ul>
+    </div>
 
     <button class="app-header-navigation__cta" type="button">
       <ButtonAppearance type="green">Reservations</ButtonAppearance>
@@ -106,12 +103,10 @@ const items = computed(() => list?.items ?? [])
 .app-header-navigation__item {
   translate: calc(-1 * var(--link-nudge)) 0 0;
   opacity: 0;
-  animation: link-hide theme('transitionDuration.200') theme('transitionTimingFunction.smooth')
-    forwards;
+  animation: link-hide theme('transitionDuration.200') theme('transitionTimingFunction.smooth') forwards;
 
   .app-header-navigation--is-open & {
-    animation: link-show theme('transitionDuration.300') theme('transitionTimingFunction.smooth')
-      var(--link-transition-delay) forwards;
+    animation: link-show theme('transitionDuration.300') theme('transitionTimingFunction.smooth') var(--link-transition-delay) forwards;
   }
 }
 
