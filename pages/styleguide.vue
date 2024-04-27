df43364a7b8630cbb17008664365910641432173
Will The Dev <hello@willthe.dev>
2024-04-27 11:27:18 +0100

    example of asymmetric box grid lolz

--- pages/styleguide.vue
+++ pages/styleguide.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <!-- <section>
-      <UiMenus />
-    </section> -->
+    <section class="grid-box wrapper">
+      <div
+        :class="[
+          'col-span-4 col-start-8',
+          'row-start-1',
+          'aspect-3/4 bg-[#CDBB9E]',
+        ]"
+      />
+
+      <div
+        :class="[
+          'col-span-5 col-start-2',
+          'row-start-3',
+          'aspect-1/1 bg-[#200E02]',
+        ]"
+      />
+
+      <div
+        :class="[
+          'col-span-4 col-start-9',
+          'row-start-7',
+          'aspect-2/3 bg-[#2D456B]',
+        ]"
+      />
+    </section>
 
     <div class="section wrapper">
       <h1 class="type-h1 opacity-40">
@@ -119,3 +141,21 @@
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.grid-box {
+  --grid-columns: 12;
+  --grid-max-width: 100vw;
+
+  @screen 3xl {
+    --grid-max-width: theme('screens.3xl');
+  }
+
+  --box-size: calc(((var(--grid-max-width) - var(--app-outer-gutter)) - (var(--app-inner-gutter) * (var(--grid-columns) - 1))) / var(--grid-columns));
+
+  display: grid;
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+  grid-template-rows: repeat(12, var(--box-size));
+  gap: var(--app-inner-gutter);
+}
+</style>
