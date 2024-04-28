6cb8ffa25845692dce856e271b80fbf389f9b93a
Michael Pumo <hello@michaelpumo.com>
2024-04-28 15:47:52 +0100

    Newsletter form subscription.

---
+++ components/form/Fieldset.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+interface Props {
+  a11y?: boolean
+  legend?: string
+}
+
+const { legend = '' } = defineProps<Props>()
+</script>
+
+<template>
+  <fieldset>
+    <legend class="sr-only">
+      {{ legend }}
+    </legend>
+
+    <slot />
+  </fieldset>
+</template>
---
+++ components/form/Input.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+interface Props {
+  id: string
+  field?:
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'hidden'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'range'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'text'
+    | 'textarea'
+    | 'time'
+    | 'url'
+    | 'week'
+}
+
+const { id, field = 'text' } = defineProps<Props>()
+const modelValue = defineModel<string>()
+
+const onInput = (event: Event) => {
+  modelValue.value = (event.target as HTMLInputElement).value
+}
+</script>
+
+<template>
+  <template v-if="field === 'textarea'">
+    <textarea
+      :id="id"
+      :class="['min-h-[110px]']"
+      :value="modelValue"
+      autocomplete="off"
+      spellcheck="false"
+      @input="onInput"
+    />
+  </template>
+
+  <template v-else>
+    <input
+      :id="id"
+      :class="[]"
+      :type="field"
+      :value="modelValue"
+      autocomplete="off"
+      spellcheck="false"
+      @input="onInput"
+    >
+  </template>
+</template>
