69b14b59d92e7c377e94b4f836a70e61f6948a3e
Michael Pumo <hello@michaelpumo.com>
2024-04-30 18:37:32 +0100

    No need for event. Bring var changes into the component.

--- pages/stories/[page].vue
+++ pages/stories/[page].vue
@@ -6,18 +6,10 @@ const story = await useStoryblokStory<PageStoryblok>(route.path)
 
 useStoryblokSetup(story)
 useNavigation(false)
-
-const setAppTheme = (theme: Luca.Theme) => {
-  document.documentElement.style.setProperty('--app-background-color', theme.background)
-  document.documentElement.style.setProperty('--app-text-color', theme.text)
-}
 </script>
 
 <template>
-  <AppTheme
-    :themes="story.content.themes"
-    @theme="setAppTheme"
-  >
+  <AppTheme :themes="story.content.themes">
     <PageCover
       v-if="story.content.cover_message"
       :message="story.content.cover_message"
