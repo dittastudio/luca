d0eee6a21865d3399aa331ac7fdb5ec408d2b3e6
Michael Pumo <hello@michaelpumo.com>
2024-07-16 21:12:55 +0100

    Update config for ESLint to be more sane.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default withNuxt(
       'antfu/top-level-function': 'off',
       'toml/indent': ['error', 2, { subTables: 1, keyValuePairs: 1 }],
       'curly': 'off',
+      'no-console': 'off',
       'node/prefer-global/process': 'off',
       'vue/define-macros-order': 'off',
       'vue/no-v-html': 'off',
@@ -23,6 +24,14 @@ export default withNuxt(
       ]],
       'vue/custom-event-name-casing': ['error', 'kebab-case', {
         ignores: [],
+      }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
       }],
     },
   }),
